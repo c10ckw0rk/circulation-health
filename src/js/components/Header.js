42fc71a71715b1e24d65a09f5fd9cc5892ae1a4b
Daniel Peterson <daniel.peterson@massive.co>
2018-05-07 22:30:52 +1000

    mobile and desktop header looking good

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import { withConsumer } from 'js/store/Store';
 import Logo from 'js/components/svg/Logo';
+import { Link } from 'react-router-dom';
 import { Hamburger } from 'js/components/icon/Hamburger';
 import './Header.scss';
+import Container from 'js/components/grid/Container';
+import { MagnifyingGlass } from 'js/components/icon/MagnifyingGlass';
 
 class Header extends React.Component {
 	state = {
-		title: ''
+		title: ['CIRCULATION', 'HEALTH'],
+		phoneTitle: 'CONSULTATION',
+		phone: '(02) 42 268 930',
+		enquiryButton: 'Make an enquiry',
+		enquiryLink: '/',
+		navItems: [
+			{
+				title: 'HOME',
+				link: '/'
+			},
+			{
+				title: 'SPECIALTY AREAS',
+				link: '/'
+			},
+			{
+				title: 'PATIENT INFORMATION',
+				link: '/'
+			},
+			{
+				title: 'LATEST NEWS',
+				link: '/'
+			},
+			{
+				title: 'ABOUT US',
+				link: '/'
+			},
+			{
+				title: 'CONTACT',
+				link: '/'
+			}
+		],
+		searchPlaceholder: 'SEARCH'
 	};
 
 	componentWillReceiveProps(nextProps, nextContent) {
@@ -17,20 +51,50 @@ class Header extends React.Component {
 	}
 
 	render() {
-		const { title } = this.state;
+		const { title, phone, phoneTitle, enquiryButton, enquiryLink, navItems, searchPlaceholder } = this.state;
+
 		return (
-			<header className={'header container-fluid'}>
-				<div className={'row inner-header'}>
-					<h1 className={'title col-xs-12'}>
-						<Logo className={'logo'} />
-						<div className={'text-wrapper'}>
-							<span className={'title-section-1 h4'}>{title[0]}</span>
-							<span className={'title-section-2'}>{title[1]}</span>
+			<header className={'header'}>
+				<Container>
+					<div className={'inner-header'}>
+						<h1 className={'title'}>
+							<Logo className={'logo'} />
+							<div className={'text-wrapper'}>
+								<span className={'title-section-1 h4'}>{title[0]}</span>
+								<span className={'title-section-2'}>{title[1]}</span>
+							</div>
+						</h1>
+						<button className={'menu'}>
+							<Hamburger />
+						</button>
+						<div className={'enquiry'}>
+							<p>
+								{phoneTitle} <br /> <a href={`tel:${phone}`}>{phone}</a>
+							</p>
 						</div>
-					</h1>
-					<button className={'menu'}>
-						<Hamburger />
-					</button>
+						<div className={'make-enquiry'}>
+							<Link to={enquiryLink} className={'button'}>
+								{enquiryButton}
+							</Link>
+						</div>
+					</div>
+				</Container>
+				<div className={'desktop-header-wrapper'}>
+					<nav className={'desktop-header'}>
+						<Container>
+							<ul className={'nav'}>
+								{navItems.map(({ title, link }, i) => (
+									<li key={i}>
+										<Link to={link}>{title}</Link>
+									</li>
+								))}
+								<li className={'search'}>
+									<MagnifyingGlass className={'icon'} />
+									<input type={'text'} placeholder={searchPlaceholder} />
+								</li>
+							</ul>
+						</Container>
+					</nav>
 				</div>
 			</header>
 		);
