ad16312a45947c29eb5c99d50ea1e814b4c93f54
Daniel Peterson <daniel.peterson@massive.co>
2018-06-23 16:03:58 +1000

    releasing app

--- build/manifest.js
+++ build/manifest.js
@@ -5,6 +5,8 @@ function Manifest() {
 	let files = fs.readdirSync(path.resolve(__dirname, '../theme/assets'));
 	files = JSON.stringify(files).replace(/(])|(\[)/gi, '');
 
+	console.log(files);
+
 	fs.writeFileSync(path.resolve(__dirname, '../theme/manifest.php'), `<?php $manifest = array(${files});`, {
 		encoding: 'utf8',
 		flag: 'w'
